8044da9ee99d960838d29aac00971d4efaa7894c
Mihai Latea <latea.alexandru@yahoo.com>
2021-10-23 21:26:17 +0300

    Added solution for 283. Move Zeroes problem

---
+++ src/com/company/MoveZeroes.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int lastZeroFoundAt = 0;
+        for(int i = 0 ; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[lastZeroFoundAt++] = nums[i];
+            }
+        }
+
+        for(int i = lastZeroFoundAt; i < nums.length; i ++) {
+            nums[i] = 0;
+        }
+    }
+}
